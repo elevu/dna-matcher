8a8ab179391460cfe19abee6009afdc585abc5f3
Eleonora Maccacaro <eleonora.maccacaro@netent.com>
2019-12-12 15:42:24 +0100

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.style.css'
+
+class App extends Component {
+    render()
+    {
+        return (
+            <div className='title'>Your DNA match here ;D</div>
+    );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin('[name].[hash].css'),
+
+    ]
+};
