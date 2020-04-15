c1bec71494f48ebaac37f1706ae33100aa0605a5
Eleonora Maccacaro <eleonora.maccacaro@netent.com>
2020-04-15 15:56:34 +0200

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            favicon: './src/assets/favicon.png'
         }),
         new ExtractTextPlugin('[name].[hash].css'),
 
