ccc914dc20a0544d4480f336b35dd3dab90fe385
Eleonora Maccacaro <eleonora.maccacaro@netent.com>
2020-04-28 22:10:06 +0200

    Add more SNPs

--- functions/src/SNPlist.ts
+++ functions/src/SNPlist.ts
@@ -1,38 +1,144 @@
 export const SNPlist = [
   {
-    name: "rs10076041",
+    name: "rs429358",
+    CC: {
+      type: "negative",
+      description: "Increased risk of Alzheimer",
+    },
     CT: {
-      type: "neutral",
-      description: "Common genotype: normal homocysteine levels",
+      type: "negative",
+      description: "Increased risk of Alzheimer",
+    },
+    TC: {
+      type: "negative",
+      description: "Increased risk of Alzheimer",
     },
     TT: {
       type: "neutral",
-      description: "Common genotype: normal homocysteine levels",
+      description: "Normal risk of Alzheimer",
     },
-    link: "https://www.snpedia.com/index.php/rs10076041",
+    link: "https://snpedia.com/index.php/Rs429358",
   },
   {
-    name: "rs4374758",
+    name: "rs12934922",
+    AA: {
+      type: "neutral",
+      description: "Normal conversion of beta-carotene to retinol",
+    },
+    AT: {
+      type: "negative",
+      description: "Reduced conversion of beta-carotene to retinol",
+    },
+    TA: {
+      type: "negative",
+      description: "Reduced conversion of beta-carotene to retinol",
+    },
+    TT: {
+      type: "negative",
+      description: "Reduced conversion of beta-carotene to retinol",
+    },
+    link: "https://snpedia.com/index.php/Rs12934922",
+  },
+  {
+    name: "rs602662",
+    AA: {
+      type: "positive",
+      description: "Higher vitamin B12 levels",
+    },
+    AG: {
+      type: "neutral",
+      description: "Average vitamin B12 levels",
+    },
+    GA: {
+      type: "neutral",
+      description: "Average vitamin B12 levels",
+    },
+    GG: {
+      type: "negative",
+      description: "Lower vitamin B12 levels",
+    },
+    link: "https://snpedia.com/index.php/Rs602662",
+  },
+  {
+    name: "rs1544410",
+    AA: {
+      type: "negative",
+      description: "Increased risk of low bone mineral density disorders",
+    },
+    AG: {
+      type: "neutral",
+      description: "Intermediate risk of low bone mineral density disorders",
+    },
+    GA: {
+      type: "neutral",
+      description: "Intermediate risk of low bone mineral density disorders",
+    },
+    GG: {
+      type: "positive",
+      description: "Decreased risk of low bone mineral density disorders",
+    },
+    link: "https://snpedia.com/index.php/Rs1544410",
+  },
+  {
+    name: "rs4654748",
+    CC: {
+      type: "negative",
+      description: "2.90 ng/mL lower Vitamin B6 blood concentration",
+    },
+    TC: {
+      type: "negative",
+      description: "1.45 ng/mL lower Vitamin B6 blood concentration",
+    },
+    CT: {
+      type: "negative",
+      description: "1.45 ng/mL lower Vitamin B6 blood concentration",
+    },
+    TT: {
+      type: "neutral",
+      description: "Normal Vitamin B6 blood concentration",
+    },
+    link: "https://snpedia.com/index.php/Rs4654748",
+  },
+  {
+    name: "rs1801133",
     CC: {
       type: "neutral",
       description: "Common genotype: normal homocysteine levels",
     },
-    TT: {
-      type: "neutral",
-      description: "Common genotype: normal homocysteine levels",
+    CT: {
+      type: "negative",
+      description:
+        "1 copy of C677T allele of MTHFR = 65% efficiency in processing folic acid",
     },
-    link: "https://www.snpedia.com/index.php/rs4374758",
-  },
-  {
-    name: "rs437488",
-    CC: {
-      type: "neutral",
-      description: "Common genotype: normal homocysteine levels",
+    TC: {
+      type: "negative",
+      description:
+        "1 copy of C677T allele of MTHFR = 65% efficiency in processing folic acid",
     },
     TT: {
+      type: "negative",
+      description:
+        "homozygous for C677T of MTHFR = 10-20% efficiency in processing folic acid = high homocysteine, low B12 and folate levels",
+    },
+    GG: {
       type: "neutral",
       description: "Common genotype: normal homocysteine levels",
     },
-    link: "https://www.snpedia.com/index.php/rs4374758",
+    GA: {
+      type: "negative",
+      description:
+        "1 copy of C677T allele of MTHFR = 65% efficiency in processing folic acid",
+    },
+    AG: {
+      type: "negative",
+      description:
+        "1 copy of C677T allele of MTHFR = 65% efficiency in processing folic acid",
+    },
+    AA: {
+      type: "negative",
+      description:
+        "homozygous for C677T of MTHFR = 10-20% efficiency in processing folic acid = high homocysteine, low B12 and folate levels",
+    },
+    link: "https://snpedia.com/index.php/Rs1801133",
   },
 ];
