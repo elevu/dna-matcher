065f37ce8dca52ab0103f96b7ca3bf7d642bf6a3
Eleonora Maccacaro <eleonora.maccacaro@netent.com>
2020-05-07 11:14:28 +0200

    Add more

--- functions/src/SNPlist.ts
+++ functions/src/SNPlist.ts
@@ -141,4 +141,418 @@ export const SNPlist = [
     },
     link: "https://snpedia.com/index.php/Rs1801133",
   },
+  {
+    name: "rs4654748",
+    CC: {
+      type: "negative",
+      description: "2.90 ng/mL lower Vitamin B6 blood concentration",
+    },
+    CT: {
+      type: "negative",
+      description: "1.45 ng/mL lower Vitamin B6 blood concentration",
+    },
+    TC: {
+      type: "negative",
+      description: "1.45 ng/mL lower Vitamin B6 blood concentration",
+    },
+    TT: {
+      type: "neutral",
+      description: "normal Vitamin B6 blood concentration",
+    },
+    GG: {
+      type: "negative",
+      description: "2.90 ng/mL lower Vitamin B6 blood concentration",
+    },
+    GA: {
+      type: "negative",
+      description: "1.45 ng/mL lower Vitamin B6 blood concentration",
+    },
+    AG: {
+      type: "negative",
+      description: "1.45 ng/mL lower Vitamin B6 blood concentration",
+    },
+    AA: {
+      type: "neutral",
+      description: "normal Vitamin B6 blood concentration",
+    },
+    link: "https://snpedia.com/index.php/Rs4654748",
+  },
+  {
+    name: "rs7501331",
+    CC: {
+      type: "normal",
+      description: "normal",
+    },
+    CT: {
+      type: "negative",
+      description: "Reduced conversion of beta-carotene to retinol",
+    },
+    TC: {
+      type: "negative",
+      description: "Reduced conversion of beta-carotene to retinol",
+    },
+    TT: {
+      type: "negative",
+      description: "Reduced conversion of beta-carotene to retinol",
+    },
+    GG: {
+      type: "normal",
+      description: "normal",
+    },
+    GA: {
+      type: "negative",
+      description: "Reduced conversion of beta-carotene to retinol",
+    },
+    AG: {
+      type: "negative",
+      description: "Reduced conversion of beta-carotene to retinol",
+    },
+    AA: {
+      type: "negative",
+      description: "Reduced conversion of beta-carotene to retinol",
+    },
+    link: "https://snpedia.com/index.php/Rs7501331",
+  },
+  {
+    name: "rs1535",
+    CC: {
+      type: "negative",
+      description:
+        "This genotype is a low converter with ~29% poorer conversion of ALA into EPA relative to the high conversion genotype (A;A)",
+    },
+    CT: {
+      type: "neutral",
+      description:
+        "This genotype is a intermediate converter and has been associated with 18.6% poorer conversion of ALA into EPA relative to the high conversion genotype (A;A).",
+    },
+    TC: {
+      type: "neutral",
+      description:
+        "This genotype is a intermediate converter and has been associated with 18.6% poorer conversion of ALA into EPA relative to the high conversion genotype (A;A).",
+    },
+    TT: {
+      type: "positive",
+      description:
+        "This genotype has been associated with the highest conversion of ALA into EPA",
+    },
+    GG: {
+      type: "negative",
+      description:
+        "This genotype is a low converter with ~29% poorer conversion of ALA into EPA relative to the high conversion genotype (A;A)",
+    },
+    GA: {
+      type: "neutral",
+      description:
+        "This genotype is a intermediate converter and has been associated with 18.6% poorer conversion of ALA into EPA relative to the high conversion genotype (A;A).",
+    },
+    AG: {
+      type: "neutral",
+      description:
+        "This genotype is a intermediate converter and has been associated with 18.6% poorer conversion of ALA into EPA relative to the high conversion genotype (A;A).",
+    },
+    AA: {
+      type: "positive",
+      description:
+        "This genotype has been associated with the highest conversion of ALA into EPA",
+    },
+    link: "https://snpedia.com/index.php/Rs1535",
+  },
+  {
+    name: "rs174537",
+    CC: {
+      type: "negative",
+      description:
+        "Associated with high arachidonic acid levels. Arachidonic acid is metabolized to both proinflammatory and anti-inflammatory eicosanoids during and after the inflammatory response.",
+    },
+    CT: {
+      type: "neutral",
+      description:
+        "Associated with intermediate arachidonic acid levels. Arachidonic acid is metabolized to both proinflammatory and anti-inflammatory eicosanoids during and after the inflammatory response.",
+    },
+    TC: {
+      type: "neutral",
+      description:
+        "Associated with intermediate arachidonic acid levels. Arachidonic acid is metabolized to both proinflammatory and anti-inflammatory eicosanoids during and after the inflammatory response.",
+    },
+    TT: {
+      type: "positive",
+      description:
+        "Associated with low arachidonic acid levels. Arachidonic acid is metabolized to both proinflammatory and anti-inflammatory eicosanoids during and after the inflammatory response.",
+    },
+    GG: {
+      type: "negative",
+      description:
+        "Associated with high arachidonic acid levels. Arachidonic acid is metabolized to both proinflammatory and anti-inflammatory eicosanoids during and after the inflammatory response.",
+    },
+    GA: {
+      type: "neutral",
+      description:
+        "Associated with intermediate arachidonic acid levels. Arachidonic acid is metabolized to both proinflammatory and anti-inflammatory eicosanoids during and after the inflammatory response.",
+    },
+    AG: {
+      type: "neutral",
+      description:
+        "Associated with intermediate arachidonic acid levels. Arachidonic acid is metabolized to both proinflammatory and anti-inflammatory eicosanoids during and after the inflammatory response.",
+    },
+    AA: {
+      type: "positive",
+      description:
+        "Associated with low arachidonic acid levels. Arachidonic acid is metabolized to both proinflammatory and anti-inflammatory eicosanoids during and after the inflammatory response.",
+    },
+    link: "https://snpedia.com/index.php/Rs174537",
+  },
+  {
+    name: "rs174548",
+    CC: {
+      type: "positive",
+      description: "Highest phosphatidylcholine values",
+    },
+    CT: {
+      type: "neutral",
+      description: "intermediate phosphatidylcholine values",
+    },
+    TC: {
+      type: "neutral",
+      description: "intermediate phosphatidylcholine values",
+    },
+    TT: {
+      type: "negative",
+      description: "low phosphatidylcholine values",
+    },
+    GG: {
+      type: "positive",
+      description: "Highest phosphatidylcholine values",
+    },
+    GA: {
+      type: "neutral",
+      description: "intermediate phosphatidylcholine values",
+    },
+    AG: {
+      type: "neutral",
+      description: "intermediate phosphatidylcholine values",
+    },
+    AA: {
+      type: "negative",
+      description: "low phosphatidylcholine values",
+    },
+    link: "https://snpedia.com/index.php/Rs174548",
+  },
+  {
+    name: "rs10741657",
+    CC: {
+      type: "negative",
+      description: "tend to have lower vitamin D levels",
+    },
+    CT: {
+      type: "neutral",
+      description: "intermediate vitamin D levels",
+    },
+    TC: {
+      type: "neutral",
+      description: "intermediate vitamin D levels",
+    },
+    TT: {
+      type: "positive",
+      description: "Tend to have higher vitamin D levels",
+    },
+    GG: {
+      type: "negative",
+      description: "tend to have lower vitamin D levels",
+    },
+    GA: {
+      type: "neutral",
+      description: "intermediate vitamin D levels",
+    },
+    AG: {
+      type: "neutral",
+      description: "intermediate vitamin D levels",
+    },
+    AA: {
+      type: "positive",
+      description: "Tend to have higher vitamin D levels",
+    },
+    link: "https://snpedia.com/index.php/Rs10741657",
+  },
+  {
+    name: "rs2060793",
+    CC: {
+      type: "positive",
+      description: "Higher serum levels of vitamin D",
+    },
+    CT: {
+      type: "neutral",
+      description: "intermediate vitamin D levels",
+    },
+    TC: {
+      type: "neutral",
+      description: "intermediate vitamin D levels",
+    },
+    TT: {
+      type: "negative",
+      description: "Lower serum levels of vitamin D",
+    },
+    GG: {
+      type: "positive",
+      description: "Higher serum levels of vitamin D",
+    },
+    GA: {
+      type: "neutral",
+      description: "intermediate vitamin D levels",
+    },
+    AG: {
+      type: "neutral",
+      description: "intermediate vitamin D levels",
+    },
+    AA: {
+      type: "negative",
+      description: "Lower serum levels of vitamin D",
+    },
+    link: "https://snpedia.com/index.php/Rs2060793",
+  },
+  {
+    name: "rs4410790",
+    CC: {
+      type: "neutral",
+      description: "likely to drink slightly more caffeine than average",
+    },
+    CT: {
+      type: "neutral",
+      description: "likely to drink slightly more caffeine than average",
+    },
+    TC: {
+      type: "neutral",
+      description: "likely to drink slightly more caffeine than average",
+    },
+    TT: {
+      type: "neutral",
+      description: "likely to drink average amounts of caffeine",
+    },
+    GG: {
+      type: "neutral",
+      description: "likely to drink slightly more caffeine than average",
+    },
+    GA: {
+      type: "neutral",
+      description: "likely to drink slightly more caffeine than average",
+    },
+    AG: {
+      type: "neutral",
+      description: "likely to drink slightly more caffeine than average",
+    },
+    AA: {
+      type: "neutral",
+      description: "likely to drink average amounts of caffeine",
+    },
+    link: "https://snpedia.com/index.php/Rs4410790",
+  },
+  {
+    name: "Rs2802292",
+    CC: {
+      type: "positive",
+      description: "1.5x~2.7x more likely to live to 100.",
+    },
+    CT: {
+      type: "positive",
+      description: "One copy of a longevity gene. Slightly increased lifespan.",
+    },
+    TC: {
+      type: "positive",
+      description: "One copy of a longevity gene. Slightly increased lifespan.",
+    },
+    TT: {
+      type: "negative",
+      description: "Less likely to live to 100.",
+    },
+    GG: {
+      type: "positive",
+      description: "1.5x~2.7x more likely to live to 100.",
+    },
+    GA: {
+      type: "positive",
+      description: "One copy of a longevity gene. Slightly increased lifespan.",
+    },
+    AG: {
+      type: "positive",
+      description: "One copy of a longevity gene. Slightly increased lifespan.",
+    },
+    AA: {
+      type: "negative",
+      description: "Less likely to live to 100.",
+    },
+    link: "https://snpedia.com/index.php/Rs2802292",
+  },
+  {
+    name: "rs5082",
+    CC: {
+      type: "negative",
+      description:
+        "Saturated fats in diet contribute to higher risk of obesity",
+    },
+    CT: {
+      type: "neutral",
+      description: "Saturated fats in diet don't to higher risk of obesity",
+    },
+    TC: {
+      type: "neutral",
+      description: "Saturated fats in diet don't to higher risk of obesity",
+    },
+    TT: {
+      type: "neutral",
+      description: "Saturated fats in diet don't to higher risk of obesity",
+    },
+    GG: {
+      type: "negative",
+      description:
+        "Saturated fats in diet contribute to higher risk of obesity",
+    },
+    GA: {
+      type: "neutral",
+      description: "Saturated fats in diet don't to higher risk of obesity",
+    },
+    AG: {
+      type: "neutral",
+      description: "Saturated fats in diet don't to higher risk of obesity",
+    },
+    AA: {
+      type: "neutral",
+      description: "Saturated fats in diet don't to higher risk of obesity",
+    },
+    link: "https://snpedia.com/index.php/Rs5082",
+  },
+  {
+    name: "rs4988235",
+    CC: {
+      type: "negative",
+      description: "Likely to be lactose intolerant as an adult",
+    },
+    CT: {
+      type: "neutral",
+      description: "Not likely to be lactose intolerant as an adult",
+    },
+    TC: {
+      type: "neutral",
+      description: "Not likely to be lactose intolerant as an adult",
+    },
+    TT: {
+      type: "neutral",
+      description: "Not likely to be lactose intolerant as an adult",
+    },
+    GG: {
+      type: "negative",
+      description: "Likely to be lactose intolerant as an adult",
+    },
+    GA: {
+      type: "neutral",
+      description: "Not likely to be lactose intolerant as an adult",
+    },
+    AG: {
+      type: "neutral",
+      description: "Not likely to be lactose intolerant as an adult",
+    },
+    AA: {
+      type: "neutral",
+      description: "Not likely to be lactose intolerant as an adult",
+    },
+    link: "https://snpedia.com/index.php/Rs4988235",
+  },
 ];
